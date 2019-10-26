4321ed61c103c0a347e37aaa530f4efc7d98a659
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2019-10-26 13:34:05 -0300

    Correção lista encadeada.

--- P2/ListaEncadeada/lista.h
+++ P2/ListaEncadeada/lista.h
@@ -1,4 +1,5 @@
 #define ERROLISTA_VAZIA -1
+#define ERRO_POS_INVALIDA -2
 
 typedef struct ele {
     void *info;
@@ -14,4 +15,12 @@ void inicializa_lista(Lista *l, int t);
 int lista_vazia(Lista *l);
 int insereNoInicio(Lista *l, void *info);
 int removeDoInicio(Lista *l, void *info);
+int insereNoFim(Lista *l, void *info);
+int removeDoFim(Lista *l, void *info);
+int insereNaPos(Lista *l, void *info, int pos);
+int removeDaPos(Lista *l, void *info, int pos);
+int modificaNaPos(Lista *l, void *info, int pos);
+int leNaPos(Lista *l, void *info, int pos);
+int insereNaOrdem(Lista *l, void *info, int (*comp) (void *, void *));
 void mostra_lista(Lista l, void (*mostra) (void *));
+void limpa_lista(Lista *l);
--- P2/ListaEncadeada/main.c
+++ P2/ListaEncadeada/main.c
@@ -1,24 +1,101 @@
 #include "lista.h"
 
-void mostra_float( void *x);
+void mostra_float(void *x);
+int compara_float(void *x, void *y);
 
-int main () {
+int main() {
     Lista l1;
     float x = 123.91239, y;
+    char r[2][11] = {"falhou!", "deu certo!"};
 
     inicializa_lista(&l1, sizeof(float));
 
-    printf("%d\n", insereNoInicio(&l1, &x));
+    printf("\nA insercao no inicio de %f %s\n", x, r[insereNoInicio(&l1, &x)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    x = 3.12;
+
+    printf("\nA insercao no inicio de %f %s\n", x, r[insereNoInicio(&l1, &x)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    x=0.6;
+
+    printf("\nA insercao no inicio de %f %s\n", x, r[insereNoInicio(&l1, &x)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    x = 5;
+
+    printf("\nA insercao no fim de %f %s\n", x, r[insereNoFim(&l1, &x)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    printf("\nA remocao do inicio de %f %s\n", y, r[removeDoInicio(&l1, &y)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    printf("\nA remocao do fim de %f %s\n", y, r[removeDoFim(&l1, &y)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    x = 9;
+
+    printf("\nA insercao no fim de %f %s\n", x, r[insereNoFim(&l1, &x)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    x = 12;
+
+    printf("\nA insercao na posicao 2 de %f %s\n", x, r[insereNaPos(&l1, &x, 2)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    printf("\nA remocao da posicao 1 de %f %s\n", y, r[removeDaPos(&l1, &y, 1)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    x = 10000000;
+
+    printf("\nA insercao ordenada de %f %s\n", x, r[insereNaOrdem(&l1, &x, compara_float)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    x = 1.2;
+
+    printf("\nA insercao ordenada de %f %s\n", x, r[insereNaOrdem(&l1, &x, compara_float)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    x = 13;
+
+    printf("\nA insercao ordenada de %f %s\n", x, r[insereNaOrdem(&l1, &x, compara_float)]);
 
     mostra_lista(l1, mostra_float);
 
-    printf("%d\n", removeDoInicio(&l1, &y));
+    x = 144000;
 
-    printf("%d\n", removeDoInicio(&l1, &y)); // erro por lista vazia
+    printf("\nA sobrescrita de %f na posicao 1 %s\n", x, r[modificaNaPos(&l1, &x, 1)]);
 
-    printf("Valor %f", y);
+    mostra_lista(l1, mostra_float);
+
+    printf("\nA leitura de %f na posicao 2 %s\n", y, r[leNaPos(&l1, &y, 2)]);
+
+    limpa_lista(&l1); // testa limpeza
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    return 0;
 }
 
-void mostra_float( void *x) {
+void mostra_float(void *x) {
     printf("%f\n", *(float*) x);
+}
+
+int compara_float(void *x, void *y) {
+    float *a = x, *b = y;
+    if (*a > *b) return 1;
+    if (*a < *b) return -1;
+    return 0;
 }
